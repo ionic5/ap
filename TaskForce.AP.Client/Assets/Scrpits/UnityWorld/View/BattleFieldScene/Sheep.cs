4938d0a3042de9379cbbd2c392e3eca11a7ae318
ionic5 <st.kaldalis@gmail.com>
2026-03-11 22:09:07 +0900

    3D Dummy sheep 추가

--- TaskForce.AP.Client/Assets/Scrpits/UnityWorld/View/BattleFieldScene/Sheep.cs
+++ TaskForce.AP.Client/Assets/Scrpits/UnityWorld/View/BattleFieldScene/Sheep.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using TaskForce.AP.Client.Core.View.BattleFieldScene;
+﻿using TaskForce.AP.Client.Core.View.BattleFieldScene;
+using System;
 using UnityEngine;
 
 namespace TaskForce.AP.Client.UnityWorld.View.BattleFieldScene
@@ -13,9 +13,7 @@ namespace TaskForce.AP.Client.UnityWorld.View.BattleFieldScene
         private bool _hasDestination;
 
         [SerializeField]
-        private Rigidbody2D _rigidbody2D;
-        [SerializeField]
-        private SpriteRenderer _spriteRenderer;
+        private Rigidbody _rigidbody;
         [SerializeField]
         private float _arrivalThreshold;
 
@@ -29,23 +27,22 @@ namespace TaskForce.AP.Client.UnityWorld.View.BattleFieldScene
         protected override void CleanUp()
         {
             base.CleanUp();
-
             ArrivedDestinationEvent = null;
+            HitEvent = null;
         }
 
         public void MoveTo(System.Numerics.Vector2 destination, float speed)
         {
             _hasDestination = true;
-            _destination = new UnityEngine.Vector3(destination.X, destination.Y, transform.position.z);
-            var direction = (_destination - transform.position).normalized;
-            _rigidbody2D.linearVelocity = direction * speed;
+            _destination = new Vector3(destination.X, 0, destination.Y);
 
-            _spriteRenderer.flipX = direction.x < 0f;
+            Vector3 direction = (_destination - transform.position).normalized;
+            _rigidbody.linearVelocity = direction * speed;
         }
 
         public void SetPosition(System.Numerics.Vector2 position)
         {
-            gameObject.transform.position = new Vector3(position.X, position.Y, gameObject.transform.position.z);
+            transform.position = new Vector3(position.X, 0, position.Y);
         }
 
         private void Update()
@@ -57,7 +54,7 @@ namespace TaskForce.AP.Client.UnityWorld.View.BattleFieldScene
                 return;
 
             _hasDestination = false;
-            _rigidbody2D.linearVelocity = Vector2.zero;
+            _rigidbody.linearVelocity = Vector3.zero;
             _destination = Vector3.zero;
 
             ArrivedDestinationEvent?.Invoke(this, EventArgs.Empty);
@@ -65,20 +62,20 @@ namespace TaskForce.AP.Client.UnityWorld.View.BattleFieldScene
 
         private bool IsArrived()
         {
-            Vector2 diff = _destination - (Vector3)_rigidbody2D.position;
+            Vector3 diff = _destination - _rigidbody.position;
             float sqrDist = diff.sqrMagnitude;
             return sqrDist < _arrivalThreshold * _arrivalThreshold;
         }
 
-        private void OnTriggerEnter2D(Collider2D other)
+        private void OnTriggerEnter(Collider other)
         {
             HitEvent?.Invoke(this, new Core.View.HitEventArgs { ObjectID = other.gameObject.name });
         }
 
         public System.Numerics.Vector2 GetPosition()
         {
-            var pos = gameObject.transform.position;
-            return new System.Numerics.Vector2(pos.x, pos.y);
+            var pos = transform.position;
+            return new System.Numerics.Vector2(pos.x, pos.z);
         }
     }
 }
